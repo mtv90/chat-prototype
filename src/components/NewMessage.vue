d7a03b333086715e4e6c602c6694797c3525993b
mtv90 <M_TV@gmx.de>
2019-03-03 18:58:17 +0100

    first commit

---
+++ src/components/NewMessage.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="new-message">
+        <form @submit.prevent="addMessage">
+            <label for="new-message">Neue Nachricht (Enter zum senden)</label>
+            <input type="text" name="new-message" v-model="newMessage" autocomplete="off">
+            <p class="red-text" v-if="feedback"> {{feedback}} </p>
+        </form>
+    </div>
+</template>
+<script>
+import db from '@/firebase/init'
+
+export default {
+    name: 'NewMessage',
+    props: ['name'],
+    data() {
+        return {
+            newMessage: null,
+            feedback: null
+        }
+    },
+    methods:{
+        addMessage(){
+            if (this.newMessage) {
+                db.collection('messages').add({
+                    content: this.newMessage,
+                    name: this.name,
+                    timestamp: Date.now()
+                }).catch(err => {
+                    console.log(err)
+                })
+                this.newMessage = null
+                this.feedback = null
+            } else {
+                this.feedback = 'Du musst eine Nachricht eingeben'
+            }
+        }
+    }
+}
+</script>
---
+++ src/components/Welcome.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="welcome container">
+    <div class="card">
+      <div class="card-content center-align">
+        <h2 class="teal-text">Willkommen</h2>
+        <form @submit.prevent="enterChat">
+          <label for="name">Gib deinen Namen ein:</label>
+          <input type="text" name="name" v-model="name" required autofocus>
+          <p v-if="feedback" class="red-text"> {{feedback}} </p>
+          <button class="btn teal">Beitreten</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Welcome',
+  data () {
+    return {
+      name: null,
+      feedback: null
+    }
+  },
+  methods: {
+    enterChat(){
+      if(this.name){
+        this.$router.push({name: 'Chat', params: {name: this.name} })
+      }else{
+        this.feedback = 'Bitte einen Namen eingeben, um beizutreten!'
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.welcome{
+  max-width: 400px;
+  margin-top: 100px;
+}
+.welcome h2{
+  font-size: 3em;
+}
+.welcome button{
+  margin: 30px auto;
+}
+</style>
